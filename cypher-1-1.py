e16f3817b5585424ea84490cfc09aae12fe45b43
Thomas Pongratz <tomp.1985@gmx.de>
2022-10-12 01:53:08 +0200

    added modul cypher-caesar-1-1

---
+++ cypher-1-1.py
@@ -0,0 +1,217 @@
+# Caesar Cipher
+import pyperclip
+try:
+    import tkinter as tk        # python v3
+except:
+    import Tkinter as tk        # python v2
+
+
+def print_translated(translated):
+    # OUTPUTS
+    lbl = tk.Label(
+        root, text = "Translated:", 
+        font=("Fira Code", 9), bg='#FAF8CC', fg='black'
+        )
+    lbl.grid(row=5, column=0, columnspan=1)
+
+    #inp = inputtxt.get(1.0, "end-1c")
+    #lbl.config(text="Provided Input: "+inp)
+    
+    # Output via Textbox 
+    text_box = tk.Text(
+        root,
+        height=8, width=20,
+        highlightthickness=0
+        )
+    text_scroll = tk.Scrollbar(orient=tk.VERTICAL,)
+
+    text_scroll.config(command=text_box.yview, )
+    text_box["yscrollcommand"] = text_scroll.set
+    
+    text_box.insert('end', translated)
+    text_box.config(state='disabled')
+    
+    text_box.grid(row=5, column=1, columnspan=2, pady=2, sticky="w")
+    text_scroll.grid(row=5, column=2, pady=2, sticky="nse")
+
+
+def get_mode(input_mode):
+    # set mode - encrypt / decrypt:
+    mode = 'encrypt'
+    mode = input_mode
+    #mode = input("Enter 'e' for encrypt, 'd' for decrypt: ")
+    if (mode == 'd'):
+        mode = 'decrypt'
+    else:
+        mode = 'encrypt'
+    return mode
+
+
+# encryption/decryption-key:
+def get_key(input_key):
+    key = 0
+    #key = int(input('\nEnter encryption-key [1 - 26]: '))
+    key = int(input_key)
+    if key == 0:
+        key = 13
+    return key
+
+
+def caesar(input_mode):
+    #message = input('Enter: ')
+    #message = input_entry.get()
+    message = input_textbox.get("1.0",'end-1c')
+    key = get_key(key_entry.get())
+    mode = get_mode(input_mode)
+
+    SYMBOLS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890 !?.'
+    #SYMBOLS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz123456789 \
+    #    !?.`~@#$%^&*()_+-=[]{}|;:<>,/'
+
+    translated = ''
+    for symbol in message:
+        if symbol in SYMBOLS:
+            symbol_index = SYMBOLS.find(symbol)
+            if mode == 'encrypt':
+                translated_index = symbol_index + key
+            elif mode == 'decrypt':
+                translated_index = symbol_index - key
+
+            if translated_index >= len(SYMBOLS):
+                translated_index = translated_index - len(SYMBOLS)
+            elif translated_index < 0:
+                translated_index = translated_index + len(SYMBOLS)
+
+            translated = translated + SYMBOLS[translated_index]
+        else:
+            translated = translated + symbol
+
+    print('\n' + message) 
+    #print('\n[ MESSAGE: ' + message + ' ]') 
+    print('\n[ KEY: ' + str(key) + ' ] [ MODE: ' + mode + ' ]: ')
+    print('\n' + translated + '\n')
+    #print('\n' + message + '  -->  ' + translated + '\n')
+    print_translated(translated)
+    pyperclip.copy(translated)
+    
+    #return translated
+
+
+# This function is called when the submit button is clicked
+def submit_callback(input_entry):
+    print("User entered : " + input_entry.get())
+    return None
+
+
+#######################  GUI ###########################
+root = tk.Tk()
+root.title("Cypher - Decypher")
+root.geometry('300x400')       #Set window size
+root.config(bg='#FAF8CC')
+
+# HEADING
+heading = tk.Label(
+    root, text="Caesar-Cypher", 
+    font=("unispace", 12), bg='#FAF8CC', fg='black'
+    )
+heading.grid(row=0, column=0, columnspan=3)
+
+heading2 = tk.Label(
+    root, text="...encrypt and decrypt messages", 
+    font=("unispace", 9), bg='#FAF8CC', fg='black'
+    )
+heading2.grid(row=1, column=0, columnspan=3)
+
+
+# INPUTS
+input_label = tk.Label(
+    root, text="Enter message:", 
+    font=("Fira Code", 9), bg='#FAF8CC', fg='black'
+    )
+input_label.grid(row=2, column=0, columnspan=1)
+
+# input_entry = tk.Entry(root)
+# input_entry.grid(row=2, column=1, columnspan=2)
+
+# Input via Textbox 
+input_textbox = tk.Text(
+    root,
+    height=8, width=20,
+    highlightthickness=0
+    )
+text_scroll = tk.Scrollbar(orient=tk.VERTICAL,)
+
+text_scroll.config(command=input_textbox.yview, )
+input_textbox["yscrollcommand"] = text_scroll.set
+
+# input_textbox.get('end', translated)
+# input_textbox.config(state='disabled')
+
+input_textbox.grid(row=2, column=1, columnspan=2, sticky="w")
+text_scroll.grid(row=2, column=2, sticky="nse")
+
+
+key_label = tk.Label(
+    root, text="Enter key:", 
+    font=("Fira Code", 9), bg='#FAF8CC', fg='black'
+    )
+key_label.grid(row=3, column=0, columnspan=1)
+
+key_entry = tk.Entry(root)
+key_entry.grid(row=3, column=1, columnspan=2)
+
+
+# # BUTTONS
+# encrypt_btn = tk.Button(
+#     root, 
+#     text="Encrypt", 
+#     command=lambda: caesar('e'),
+#     font=("Fira Code", 9), bg='#444444', fg='#F04F5A'
+#     )
+# encrypt_btn.grid(row=3, column=1)
+
+# decrypt_btn = tk.Button(
+#     root, 
+#     text="Decrypt", 
+#     command=lambda: caesar('d'), 
+#     font=("Fira Code", 9), bg='#444444', fg='#F04F5A'
+#     )
+# decrypt_btn.grid(row=3, column=2)
+
+
+#####################  FAKE-BUTTONS #########################
+# FAKE-BUTTONS
+def OnPressed_encrypt(event):
+    caesar('e')
+def OnHover(event):
+    encrypt_But.config(bg='#F04F5A', fg='#444444')
+def OnLeave(event):
+    encrypt_But.config(bg='white', fg='black')
+
+encrypt_But = tk.Label(root, text='Encrypt', bg='white', relief='groove')
+#encrypt_But.place(x=10, y=10, width=100)
+encrypt_But.grid(row=4, column=1, padx=5, pady=5)
+encrypt_But.bind('<Button-1>', OnPressed_encrypt)
+encrypt_But.bind('<Enter>', OnHover)
+encrypt_But.bind('<Leave>', OnLeave)
+
+
+def OnPressed_decrypt(event):
+    caesar('d')
+def OnHover(event):
+    decrypt_But.config(bg='#F04F5A', fg='#444444')
+def OnLeave(event):
+    decrypt_But.config(bg='white', fg='black')
+
+decrypt_But = tk.Label(root, text='Decrypt', bg='white', relief='groove')
+#decrypt_But.place(x=10, y=10, width=100)
+decrypt_But.grid(row=4, column=2, pady=5)
+decrypt_But.bind('<Button-1>', OnPressed_decrypt)
+decrypt_But.bind('<Enter>', OnHover)
+decrypt_But.bind('<Leave>', OnLeave)
+#############################################################
+
+
+# MAIN
+root.mainloop()
+#############################################################
